2f06e62db07f745ef74fb66b29caa80a40b3c252
Tomas luquez <luquez1431@gmail.com>
2025-06-23 13:17:11 -0300

    export app

--- index.ts
+++ index.ts
@@ -3,11 +3,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { syncDatabase } from "./Database/sync";
 
-
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -16,7 +14,6 @@ app.get('/', (_req, res) => {
     res.send('API funcionando correctamente');
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`),
-        syncDatabase();
-});
+syncDatabase();
+
+export default app;
