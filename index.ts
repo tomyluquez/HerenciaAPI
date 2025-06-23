7836241c40a7287d59bdede9f004c0d85818c055
Tomas luquez <luquez1431@gmail.com>
2025-06-23 13:00:58 -0300

    src-index

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { syncDatabase } from "../Database/sync";
+import { syncDatabase } from "./Database/sync";
 
 
 dotenv.config();
